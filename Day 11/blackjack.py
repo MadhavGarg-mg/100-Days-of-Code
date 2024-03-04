5f0f5c77eb79cb56cca929ce4c9e4d7f1e5d77f6
Madhavgarg-mg <c2064676@newcastle.ac.uk>
2024-03-04 14:34:44 +0000

    initial commit

---
+++ Day 11/blackjack.py
@@ -0,0 +1,128 @@
+"""I will make a blackjack game today"""
+import random
+
+def check_eleven(card: list, total: int):
+    """This deals with the functionality of ace which can be 11 or 1 depending on the situation."""
+    if 11 in card and total > 21:
+        card[-1] = 1
+        total -= 10
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10] * 16
+
+print('Welcome to Blackjack!\n')
+
+total_money = int(input('How much money are you willing to play with?\n'))
+if total_money < 1:
+    print('Are you trying to waste my time! Get out!')
+    exit()
+
+while True:
+    bet_amount = int(input('How much would you like to bet?\n'))
+    if bet_amount < 1:
+        print('Are you trying to waste my time! Get out!')
+        break
+    while bet_amount > total_money:
+        print("Don't be cheeky! You are broke and can't afford that. Try again.")
+        print(f"You have {total_money}\n")
+        bet_amount = int(input('How much would you like to bet?\n'))
+
+    comp_cards = []
+    player_cards = []
+
+    comp_cards.append(random.choice(cards))
+    player_cards.append(random.choice(cards))
+    comp_cards.append(random.choice(cards))
+    player_cards.append(random.choice(cards))
+
+    comp_total = 0
+    player_total = 0
+
+    for i in player_cards:
+        player_total += i
+        check_eleven(player_cards, player_total)
+
+    print(f'Your cards are {player_cards}')
+    print(f"The computer's card is {[comp_cards[0]]}")
+    print(f'Your total is {player_total}')
+
+    if player_total != 21:
+
+        while True:
+            more_cards = input('Do you want another card?: Y or N \n').lower()
+
+            if more_cards == 'y':
+                player_cards.append(random.choice(cards))
+                player_total += player_cards[-1]
+                check_eleven(player_cards, player_total)
+                if player_total > 21:
+                    break
+                print(f'Your total is {player_total}')
+                print(f'Your cards are {player_cards}')
+            else:
+                break
+
+        if player_total > 21:
+            print(f'Your cards are {player_cards}')
+            print(f'Your total is {player_total}')
+            print(f'Sorry you bust and lost {bet_amount}')
+            total_money -= bet_amount
+
+        else:
+            for i in comp_cards:
+                comp_total += i
+                if 11 in comp_cards and comp_total > 21:
+                    comp_cards[-1] = 1
+                    comp_total -= 10
+            print(f"The computer's cards are {comp_cards}")
+
+            while comp_total < 17:
+                comp_cards.append(random.choice(cards))
+                comp_total += comp_cards[-1]
+                if comp_cards[-1] == 11 and comp_total > 21:
+                    comp_cards[-1] = 1
+                    comp_total -= 10
+                print(f"The computer's cards are {comp_cards}")
+
+            print(f'The computer total is {comp_total}')
+            print(f'Your total is {player_total}')
+
+            if comp_total > 21:
+                print(f'Congratulations! You won and doubled your bet of {bet_amount}')
+                total_money += bet_amount
+            else:
+                if comp_total > player_total:
+                    total_money -= bet_amount
+                    print(f'You lost {bet_amount}')
+                elif player_total > comp_total:
+                    print(f'Congratulations! You won and doubled your bet of {bet_amount}')
+                    total_money += bet_amount
+                elif player_total == comp_total:
+                    print('It is a tie. You get your bet back.')
+        print(f'Your total money now is {total_money}')
+
+    else:
+        for i in comp_cards:
+            comp_total += i
+            check_eleven(comp_cards, comp_total)
+        if comp_total == 21:
+            print('It is a tie. You get your money back.')
+        else:
+            print('Congratulations! You won! You got a Blackjack!')
+            total_money += (1.5 * bet_amount)
+
+    if total_money == 0:
+        print('You ran out of money! The ATM is right there.')
+        get_money = input('Do you want to get more money?: Y or N').lower()
+        if get_money != 'y':
+            print('Thank you for playing black jack. Hope to see you soon.')
+            break
+        else:
+            total_money = int(input('Welcome back! How much money do you have now? \n'))
+            if total_money < 1:
+                print('Are you trying to waste my time! Get out!')
+                break
+    else:
+        play_again = input('Play again? Y or N: \n')
+        if play_again.lower() != 'y':
+            print(f'Thank you for playing black jack. Your total amount is {total_money}.')
+            break
